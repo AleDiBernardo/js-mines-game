161d58a752a285296e64e6f634214caf7a847032
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-09 15:20:04 +0200

    feat: created grid and add event listener to start button and cells

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,21 @@
-console.log("function");
+function createGrid() {
+  if (startClickCounter > 0) {
+  } else {
+    for (let i = 0; i < dimension; i++) {
+      cell = document.createElement("div");
+      cell.classList.add("cell", "flex", "just-cont-ctr");
+      cell.innerHTML = i + 1;
+      cell.addEventListener("click", cellClicked);
+      playground.append(cell);
+    }
+  }
+  startClickCounter++;
+}
+
+function cellClicked() {
+  if (startClickCounter > 0) {
+    this.style.backgroundColor = "lightsalmon";
+  }
+  console.log("cella: " + this.innerHTML);
+  this.style.backgroundColor = "blue";
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("ciao");
+const startBtn = document.getElementById("start-game");
+let playground = document.getElementById("playground");
+let cell;
+
+let dimension = 100;
+let startClickCounter=0;
+
+startBtn.addEventListener("click", createGrid);
